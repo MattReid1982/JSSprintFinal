7404be9011517f02f9e837790f4b5aa04a57f4eb
Justin Seaward <jut_seaward@yahoo.com>
2025-08-15 10:18:33 -0230

    images name appened to the correct names...

--- index.js
+++ index.js
@@ -106,12 +106,12 @@ document.getElementById("storyBtn").addEventListener("click", function(){
 // Banner images.
 let step = 0;
 let Images = new Array();
-Images[0] = "Images/BannerImages1.jpg";
-Images[1] = "Images/BannerImages2.jpeg";
-Images[2] = "Images/BannerImages3.jpeg";
-Images[3] = "Images/BannerImages4.jpg";
-Images[4] = "Images/BannerImages5.jpeg";
-Images[5] = "Images/BannerImages6.jpeg";
+Images[0] = "Images/BannerImage1.jpg";
+Images[1] = "Images/BannerImage2.jpeg";
+Images[2] = "Images/BannerImage3.jpeg";
+Images[3] = "Images/BannerImage4.jpg";
+Images[4] = "Images/BannerImage5.jpeg";
+Images[5] = "Images/BannerImage6.jpeg";
 
 window.onload = setInterval(gallery, 3000);
 
